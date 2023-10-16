44c2fde56407fca41122a7bed083108d184bb891
Esteban <estebitangb12@gmail.com>
2023-10-16 16:38:09 +0200

    changes

--- app/src/main/java/com/example/peliculas/MainActivity.kt
+++ app/src/main/java/com/example/peliculas/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.peliculas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("#gec","QAWeqewqweqweqweqwe")
     }
 }
